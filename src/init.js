4ccd2738448f55999eb2ae62d85911a6bec3d492
Vaibhav Nayak <vbv.nyk.2003@gmail.com>
2022-12-02 23:46:29 +0530

    Add switch project

---
+++ src/init.js
@@ -0,0 +1,29 @@
+import Project from "./newProject";
+let curCount = 0;
+
+export default function init(){
+    const addTab = document.querySelector(".add-tab");
+    const addTodo = document.querySelector(".add-todo");
+    let count = 0;
+    let project = new Project("Project 1",0);
+    let projects = [];
+    projects[0] = project;
+    project.loadTodos();
+    addTab.addEventListener("click",()=>{
+        count++;
+        project = new Project(`Project ${count + 1}`,count);
+        projects[count] = project; 
+        projects[count].loadTodos();
+        curCount = count;
+    });    
+    addTodo.addEventListener("click",()=>{
+        console.log(curCount);
+        console.log(projects);
+        projects[curCount].addTodo(`New Todo`);
+        projects[curCount].loadTodos();
+    }); 
+} 
+
+export function changeProject(e){
+    curCount = e.target.dataset.count;
+}
---
+++ src/newProject.js
@@ -0,0 +1,44 @@
+'use strict';
+
+import init, { changeProject } from "./init";
+
+export default function Project(name,count){
+    let todos = ['New Todo'];
+    const workName = document.querySelector(".work-name");
+    const workContainer = document.querySelector(".work-container");
+    const tabBar = document.querySelector(".tab-bar");
+    const newTab = document.createElement("div");
+
+    function addTodo(todoName){
+        todos.push(todoName); 
+        loadTodos();  
+    }   
+
+    function loadTodos(){
+        let cur = 1;
+        while(workContainer.firstElementChild){
+            workContainer.firstElementChild.remove();
+        }
+        for(let todoName of todos){
+            const todo = document.createElement("div");
+            todo.textContent = `${cur}. ${todoName}`;
+            workContainer.appendChild(todo);
+            todo.classList.add("todo-item");
+            cur++;
+        }
+    }
+    const loadProject = (function (){
+        workName.textContent = name;
+        newTab.textContent = name;
+        newTab.classList.add("tab-item");
+        newTab.setAttribute("data-count",count);
+        newTab.addEventListener("click",(e)=>{
+            changeProject(e);
+            workName.textContent = name;
+            loadTodos();
+        })
+        tabBar.appendChild(newTab);
+    })();
+
+    return {addTodo,loadTodos,name};
+}
